b4139152c86911c4551d89c966e4fb01ae50af03
Regynald Augustin <Raugustin95@gmail.com>
2014-02-05 16:29:35 -0500

    Revert 69bac8f..9034b9e
    This rolls back to commit 69bac8f2f0b85f3be8bc9a01a06dca30bb322f54.

--- main.java
+++ main.java
@@ -4,11 +4,7 @@ public class main
 
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 
-=======
-		System.out.println(library.reverse("hi"));
->>>>>>> parent of e42fbee... Problems 5 and 6
 	}
 
 }
--- p006.java
+++ p006.java
@@ -1,14 +1,17 @@
 //Run Time: 0ms
-public class p001 {
+public class p006 {
+
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		int result=0,i=0;
-		while(i<1000) {
-			if(i%3==0||i%5==0)
-				result+=i;
-			i++;
-		}
+		int result=0,square=0,sum=0;
+		for(int i=1;i<=100;i++)
+			square+=i*i;
+		for(int j=1;j<=100;j++)
+			sum+=j;
+		sum*=sum;
+		result=sum-square;
 		System.out.println(result);
 		System.out.println(System.currentTimeMillis()-start+"ms");
 	}
+
 }
